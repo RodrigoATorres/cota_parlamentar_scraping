df4bffa1f066d6e3dfa4c7ed5bf150e4d3eea7fa
Rodrigo Torres <ratorres7@gmail.com>
2020-11-18 15:48:51 -0300

    Alterada maneira de popular banco de dados, evitando duplicadas e aumentando a velocidade

--- src/controllers/json2Db.js
+++ src/controllers/json2Db.js
@@ -3,13 +3,16 @@ const dbObj = require('./dbConnection')
 
 module.exports = async (filePath) =>{
     let jsonData = JSON.parse(fs.readFileSync(filePath, 'utf-8')).dados
-    let newDocs = []
-    let res
-    let count = 0
 
     const result = await dbObj.collection('despesas').createIndex({ idDocumento: 1 }, { unique: true });
     console.log(result)
-    let tmpRes = await dbObj.collection('despesas').insertMany(jsonData, { ordered: false } )
-    console.log(tmpRes.insertedIds)
 
+    try{
+        let tmpRes = await dbObj.collection('despesas').insertMany(jsonData, { ordered: false } )
+        console.log(`All rows inserted.`)
+    }
+    catch (e){
+        console.log(`${e.result.result.nInserted} rows inserted. ${e.writeErrors.length} rows not inserted`)
+    }
+    return 'Sucess'
 }
