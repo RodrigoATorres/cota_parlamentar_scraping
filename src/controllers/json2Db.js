592a0aad3d3986829b4c334f1bb2f1b16d771ed3
Rodrigo Torres <ratorres7@gmail.com>
2020-11-19 12:59:12 -0300

    Adicionado codigo para importaçao de dados de secretarios e json com os respectivos dados

--- src/controllers/json2Db.js
+++ src/controllers/json2Db.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const dbObj = require('./dbConnection')
 
-module.exports = async (filePath) =>{
+module.exports.importExpenses = async (filePath) =>{
     let jsonData = JSON.parse(fs.readFileSync(filePath, 'utf-8')).dados
 
     const result = await dbObj.collection('despesas').createIndex({ idDocumento: 1 }, { unique: true });
@@ -9,6 +9,23 @@ module.exports = async (filePath) =>{
 
     try{
         let tmpRes = await dbObj.collection('despesas').insertMany(jsonData, { ordered: false } )
+        console.log(`All rows inserted.`)
+    }
+    catch (e){
+        console.log(`${e.result.result.nInserted} rows inserted. ${e.writeErrors.length} rows not inserted`)
+    }
+    return 'Sucess'
+}
+
+
+module.exports.importSecretaries = async (filePath) =>{
+    let jsonData = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+
+    const result = await dbObj.collection('secretarios').createIndex({ 'Nome do Secretário': 'text' }, { unique: true });
+    console.log(result)
+
+    try{
+        let tmpRes = await dbObj.collection('secretarios').insertMany(jsonData, { ordered: false } )
         console.log(`All rows inserted.`)
     }
     catch (e){
